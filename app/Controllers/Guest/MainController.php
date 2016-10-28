63824a462e7aaaee2c8cc4ebacae390654f4bab4
Antony Kalogeropoulos <anthonykalogeropoulos@gmail.com>
2016-10-29 00:17:02 +0300

    Create all Views

--- app/Controllers/Guest/MainController.php
+++ app/Controllers/Guest/MainController.php
@@ -9,4 +9,24 @@ class MainController extends Controller
 	{
 		return $this->view('home');
 	}
+
+	public function about()
+	{
+		return $this->view('about');
+	}
+
+	public function contact()
+	{
+		return $this->view('contact');
+	}
+
+	public function gallery()
+	{
+		return $this->view('gallery');
+	}
+
+	public function single()
+	{
+		return $this->view('single');
+	}
 }
--- routes/guest.php
+++ routes/guest.php
@@ -1,3 +1,9 @@
 <?php
 
 $router->get('', 'Guest/MainController@home');
+$router->get('home', 'Guest/MainController@home');
+$router->get('index', 'Guest/MainController@home');
+$router->get('about', 'Guest/MainController@about');
+$router->get('single', 'Guest/MainController@single');
+$router->get('contact', 'Guest/MainController@contact');
+$router->get('gallery', 'Guest/MainController@gallery');
